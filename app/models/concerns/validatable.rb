d30f5801a394e7665436ccfa60026ead2128be01
rahul <rahulp.tout@gmail.com>
2022-08-20 19:46:41 +0530

    20-08-2022

---
+++ app/models/concerns/validatable.rb
@@ -0,0 +1,10 @@
+module Validatable 
+	extend ActiveSupport::Concern
+  included do 
+     # paste validation when similar do file like student.rb
+
+     # add --- include Validatable ---- in model file like student.rb file 
+  end	
+
+
+end
